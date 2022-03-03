ab1c873eb87d20f27c78115c5796b6c1d9bc81fa
YitongXia <xiayitong0630@gmail.com>
2022-03-03 22:13:07 +0100

    initial commit

---
+++ hw02/cpp/cmake-build-debug/CMakeFiles/cjdemo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cjdemo.dir/src/main.cpp.o"
+  "CMakeFiles/cjdemo.dir/src/main.cpp.o.d"
+  "cjdemo"
+  "cjdemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cjdemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
